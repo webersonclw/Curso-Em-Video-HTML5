d7ed8fedfc9e20754afb58d7323fec856355ed77
Weberson Santos (TELÓ) <91290514+webersonclw@users.noreply.github.com>
2021-10-03 17:34:23 -0300

    Update funcoes.js

--- _javascript/funcoes.js
+++ _javascript/funcoes.js
@@ -4,13 +4,19 @@
     <title>Teste  JavaScript</title>
     <meta charset="UTF-8">
     <script>
-        function acendeLampada() {document.getElementById("luz").src="_imagens/lampada-acesa.jpg";}
-        function apagaLampada() {document.getElementById("luz").scr="_imagens/lampada-apagada.jpg";}
-        function quebralLamapada () {document.getElementById("luz").src="_imagens/lamapada-quebrada.jpg";}
+        var quebrada = false;
+        function MudaLampada(tipo) {
+            if (!quebrada) {
+                document.getElementById("luz").src = "_imagens/" + tipo + ".jpg";
+                if (tipo == 'lampada-quebrada') {
+                    quebrada = true;
+                }
+            }
+        }
     </script>
 </head>
 <body>
     <h1>Acenda a Lâmpada!</h1>
-    <img src="_imagens/lampada-apagada.jpg" id="luz" onmousemove="acendeLampada()" onmouseout="apagaLampada()" onclick="quebralLamapada()" />
+    <img src="_imagens/lampada-apagada.jpg" id="luz" onmousemove="MudaLampada('lampada-acesa')" onmouseout="MudaLampada('lampada-apagada')" onclick="MudaLampada('lampada-quebrada')"/>
 </body>
 </html>
